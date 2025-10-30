7d305875ee431a53e3bf9d3135f75ed907f363b7
tasneemramadan947-hue <tasneemramadan947@gmail.com>
2025-10-30 03:32:16 +0300

    Add files via upload

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+
+const Color tdRed = Color(0xFF0D47A1); 
+const Color tdBlue = Color(0xFF42A5F5);  
+const Color tdBlack = Color(0xFF1E3A5F); 
+const Color tdGrey = Color(0xFF90CAF9);  
+const Color tdBGColor = Color(0xFFFFFFFF);
